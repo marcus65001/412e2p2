f57ac424e57a421a772d7f53b832955e644b903d
marcus65001 <41275811+marcus65001@users.noreply.github.com>
2023-02-12 14:03:31 -0700

    Update led_node.py

--- packages/e2/src/led_node.py
+++ packages/e2/src/led_node.py
@@ -10,10 +10,6 @@ from std_msgs.msg import String
 class LedNode(DTROS):
 
     def __init__(self, node_name):
-        """Wheel Encoder Node
-        This implements basic functionality with the wheel encoders.
-        """
-
         # Initialize the DTROS parent class
         super(LedNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
         self.veh_name = rospy.get_namespace().strip("/")
@@ -47,10 +43,16 @@ class LedNode(DTROS):
         self.srvp_led_emitter(msgp)
         return LEDSetResponse()
 
-
+    def run(self):
+        r = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            if not rosnode.rosnode_ping("task_p2_node"):
+                rospy.signal_shutdown("done")
+            r.sleep()
 
 if __name__ == '__main__':
     node = LedNode(node_name='led_node')
     # Keep it spinning to keep the node alive
     rospy.loginfo("led_node is up and running...")
+    node.run()
     rospy.spin()
